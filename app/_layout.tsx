3fc0be626cbcbf072d0ae8bf78302bd1c4143d5a
Oyabure Zakari <oyabure205@gmail.com>
2025-02-01 19:54:26 +0100

    implent tabs navigation and create auth folder

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -23,8 +23,8 @@ export default function RootLayout() {
 
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="(tabs)" />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="auth" options={{ headerShown: false }} />
     </Stack>
   );
 }
