b6ab7515f2e8c24eb705d26b5b11b4cbeed44aa5
Oyabure Zakari <oyabure205@gmail.com>
2025-02-01 21:45:40 +0100

    add tab bars icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,32 +1,61 @@
 import React from "react";
 import { Tabs } from "expo-router";
 import { COLORS } from "@/constants/colors";
+import { MaterialCommunityIcons, AntDesign, Ionicons } from "@expo/vector-icons";
 
 export default function TabsLayout() {
   return (
     <Tabs screenOptions={
       {
         headerShown:false,
-        tabBarActiveTintColor: COLORS.primaryBlack,
+        tabBarActiveTintColor: COLORS.backgroundGrey,
+        tabBarInactiveTintColor: COLORS.SecondaryBlack,
         tabBarShowLabel:false,
         tabBarStyle: {
-          backgroundColor: COLORS.backgroundGrey,
+          backgroundColor: COLORS.primaryBlack,
         }
       }
       }>
       <Tabs.Screen 
         name="index" 
-        options={{ tabBarLabel: "Dictionary" }} 
+        options={{ 
+          tabBarLabel: "Dictionary",
+          tabBarIcon:({color, size}) => (
+            <MaterialCommunityIcons
+              name="book-open-page-variant-outline"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
       />
 
       <Tabs.Screen 
         name="Translate" 
-        options={{ tabBarLabel: "Translate" }} 
+        options={{ 
+          tabBarLabel: "Translate",
+          tabBarIcon:({color, size}) => (
+            <AntDesign
+              name="camerao"
+              color={color}
+              size={size}
+            />
+          ),
+        }} 
       />
 
       <Tabs.Screen
         name="Translation"
-        options={{ tabBarLabel: "Translation" }}
+        options={{ 
+          tabBarLabel: "Translation",
+          tabBarIcon:({color, size}) => (
+            <Ionicons
+              name="language"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
       />
     </Tabs>
   );
--- components/onboarding/OnboardingUI.tsx
+++ components/onboarding/OnboardingUI.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, StyleSheet, TouchableOpacity, View } from "react-native";
+import { StyleSheet} from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaView } from "react-native-safe-area-context";
 
