f865e9755cd4af89c31fdb5176f911033e88c5bf
Oyabure Zakari <oyabure205@gmail.com>
2025-02-08 21:43:44 +0100

    create firebase config file and update splash screen BG in app.json file

---
+++ firebaseConfig.ts
@@ -0,0 +1,17 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBnLDlABi_x3Kc-PGYRVHIh3aRWx73GRIc",
+//   authDomain: "larai-193af.firebaseapp.com",
+//   projectId: "larai-193af",
+//   storageBucket: "larai-193af.firebasestorage.app",
+//   messagingSenderId: "516013678456",
+//   appId: "1:516013678456:web:864999dae0dba893f7328d"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
