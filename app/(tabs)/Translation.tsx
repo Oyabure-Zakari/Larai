633124ff25932b477d7fd0fd0fe483ee22c9bc70
Oyabure Zakari <oyabure205@gmail.com>
2025-02-18 14:16:35 +0100

    implement translation functionality

--- app/(tabs)/Translation.tsx
+++ app/(tabs)/Translation.tsx
@@ -9,28 +9,138 @@ import { TextInput } from "react-native";
 import { COLORS } from "@/constants/colors";
 import { Ionicons } from "@expo/vector-icons";
 import { TouchableOpacity } from "react-native";
+import axios from "axios";
+
+import { Picker } from "@react-native-picker/picker";
 
 export default function Translation() {
+  const [word, setWord] = useState("");
+  const [query, setQuery] = useState("");
   const [translation, setTranslation] = useState("");
+  const [translateFrom, setTranslateFrom] = useState("");
+  const [translateTo, setTranslateTo] = useState("");
+
+  const sendBtn = async () => {
+    const options = {
+      method: "POST",
+      url: "https://free-google-translator.p.rapidapi.com/external-api/free-google-translator",
+      params: {
+        from: translateFrom,
+        to: translateTo,
+        query: word,
+      },
+      headers: {
+        "x-rapidapi-key": "bdb64cf2eemsh8a6592eeb408bcfp122f74jsn0e7790dcb96a",
+        "x-rapidapi-host": "free-google-translator.p.rapidapi.com",
+        "Content-Type": "application/json",
+      },
+      data: {
+        translate: "rapidapi",
+      },
+    };
+
+    try {
+      const response = await axios.request(options);
+      console.log(response.data);
+      setQuery(response.data.query);
+      setTranslation(response.data.translation);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.text}>Translation</Text>
+
+      <Text>Translation From</Text>
+      {/* translate from */}
+      <Picker
+        selectedValue={translateFrom}
+        onValueChange={(value) => setTranslateFrom(value)}
+        style={styles.picker}
+      >
+        <Picker.Item label="Select a language" value="" enabled={false} />
+        <Picker.Item label="English 🇬🇧🇺🇸🇦🇺" value="en" />
+        <Picker.Item label="Chinese (Mandarin) 🇨🇳" value="zh" />
+        <Picker.Item label="Spanish 🇪🇸🇲🇽🇦🇷" value="es" />
+        <Picker.Item label="Hindi 🇮🇳" value="hi" />
+        <Picker.Item label="Arabic 🇸🇦🇪🇬🇦🇪" value="ar" />
+        <Picker.Item label="French 🇫🇷🇨🇦🇧🇪" value="fr" />
+        <Picker.Item label="Bengali 🇧🇩🇮🇳" value="bn" />
+        <Picker.Item label="Portuguese 🇵🇹🇧🇷" value="pt" />
+        <Picker.Item label="Russian 🇷🇺" value="ru" />
+        <Picker.Item label="Urdu 🇵🇰🇮🇳" value="ur" />
+        <Picker.Item label="German 🇩🇪🇦🇹🇨🇭" value="de" />
+        <Picker.Item label="Japanese 🇯🇵" value="ja" />
+        <Picker.Item label="Swahili 🇰🇪🇹🇿🇺🇬" value="sw" />
+        <Picker.Item label="Italian 🇮🇹" value="it" />
+        <Picker.Item label="Turkish 🇹🇷" value="tr" />
+        <Picker.Item label="Korean 🇰🇷" value="ko" />
+        <Picker.Item label="Tamil 🇮🇳🇱🇰🇸🇬" value="ta" />
+        <Picker.Item label="Persian (Farsi) 🇮🇷" value="fa" />
+        <Picker.Item label="Vietnamese 🇻🇳" value="vi" />
+        <Picker.Item label="Polish 🇵🇱" value="pl" />
+        <Picker.Item label="Dutch 🇳🇱🇧🇪" value="nl" />
+        <Picker.Item label="Filipino (Tagalog) 🇵🇭" value="tl" />
+        <Picker.Item label="Thai 🇹🇭" value="th" />
+        <Picker.Item label="Greek 🇬🇷" value="el" />
+        <Picker.Item label="Hebrew 🇮🇱" value="he" />
+      </Picker>
+
+      <Text>Translation To</Text>
+
+      {/* translate to */}
+            <Picker
+        selectedValue={translateTo}
+        onValueChange={(value) => setTranslateTo(value)}
+        style={styles.picker}
+      >
+        <Picker.Item label="Select a language" value="" enabled={false} />
+        <Picker.Item label="English 🇬🇧🇺🇸🇦🇺" value="en" />
+        <Picker.Item label="Chinese (Mandarin) 🇨🇳" value="zh" />
+        <Picker.Item label="Spanish 🇪🇸🇲🇽🇦🇷" value="es" />
+        <Picker.Item label="Hindi 🇮🇳" value="hi" />
+        <Picker.Item label="Arabic 🇸🇦🇪🇬🇦🇪" value="ar" />
+        <Picker.Item label="French 🇫🇷🇨🇦🇧🇪" value="fr" />
+        <Picker.Item label="Bengali 🇧🇩🇮🇳" value="bn" />
+        <Picker.Item label="Portuguese 🇵🇹🇧🇷" value="pt" />
+        <Picker.Item label="Russian 🇷🇺" value="ru" />
+        <Picker.Item label="Urdu 🇵🇰🇮🇳" value="ur" />
+        <Picker.Item label="German 🇩🇪🇦🇹🇨🇭" value="de" />
+        <Picker.Item label="Japanese 🇯🇵" value="ja" />
+        <Picker.Item label="Swahili 🇰🇪🇹🇿🇺🇬" value="sw" />
+        <Picker.Item label="Italian 🇮🇹" value="it" />
+        <Picker.Item label="Turkish 🇹🇷" value="tr" />
+        <Picker.Item label="Korean 🇰🇷" value="ko" />
+        <Picker.Item label="Tamil 🇮🇳🇱🇰🇸🇬" value="ta" />
+        <Picker.Item label="Persian (Farsi) 🇮🇷" value="fa" />
+        <Picker.Item label="Vietnamese 🇻🇳" value="vi" />
+        <Picker.Item label="Polish 🇵🇱" value="pl" />
+        <Picker.Item label="Dutch 🇳🇱🇧🇪" value="nl" />
+        <Picker.Item label="Filipino (Tagalog) 🇵🇭" value="tl" />
+        <Picker.Item label="Thai 🇹🇭" value="th" />
+        <Picker.Item label="Greek 🇬🇷" value="el" />
+        <Picker.Item label="Hebrew 🇮🇱" value="he" />
+      </Picker>
+
+      <Text>{query}</Text>
+
+      <Text>{translation}</Text>
 
       <View style={styles.textAndButtonView}>
         <TextInput
           style={styles.textInput}
           placeholder="dictionary"
           placeholderTextColor="grey"
-          value={translation}
-          onChangeText={setTranslation}
+          value={word}
+          onChangeText={setWord}
         />
 
-<TouchableOpacity style={styles.sendBtn}>
-      <Ionicons name="send" size={20} color={COLORS.primaryBlack} />
-    </TouchableOpacity>
+        <TouchableOpacity style={styles.sendBtn} onPress={sendBtn}>
+          <Ionicons name="send" size={20} color={COLORS.primaryBlack} />
+        </TouchableOpacity>
       </View>
-
-
     </SafeAreaView>
   );
 }
@@ -42,6 +152,10 @@ const styles = StyleSheet.create({
     alignItems: "center",
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
+
+  picker: { 
+    paddingVertical: 20,
+    width: '100%' },
 
   text: {
     fontSize: FONT_SIZE.mainText_Seoge.large,
@@ -57,8 +171,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     paddingHorizontal: 4,
-    backgroundColor: "red",
-
+    //backgroundColor: "red",
   },
 
   textInput: {
