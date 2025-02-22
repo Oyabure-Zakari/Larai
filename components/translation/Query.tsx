8de58c4707bfc23583ad72f7ae62ea404190a0b5
Oyabure Zakari <oyabure205@gmail.com>
2025-02-22 18:03:05 +0100

    create Query component

---
+++ components/translation/Query.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import { COLORS } from "@/constants/colors";
+import { FONT_SIZE } from "@/constants/fonts";
+import { useTranslationStore } from "@/store/useTranslationStore";
+
+export default function Query() {
+  const query = useTranslationStore((state) => state.query);
+
+  return (
+    <View style={styles.queryView}>
+      <Text style={styles.queryText}>{query}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  queryView: {
+    padding: 20,
+    width: "90%",
+    marginTop: 20,
+    marginLeft: 16,
+    alignSelf: "flex-end",
+    borderTopEndRadius: 10,
+    borderTopStartRadius: 10,
+    borderBottomRightRadius: 10,
+  },
+
+  queryText: {
+    marginTop: 60,
+    textAlign: "right",
+    fontFamily: "segoeui_bold",
+    color: COLORS.backgroundColor,
+    fontSize: FONT_SIZE.consolas.small,
+  },
+});
