ae13945f129ed0b8863e87e2161fec0335a18a80
Oyabure Zakari <oyabure205@gmail.com>
2025-02-22 17:44:02 +0100

    create text And Button component

---
+++ components/translation/TextAndButton.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function TextAndButton() {
+  return (
+    <View>
+      <Text>TextAndButton</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
