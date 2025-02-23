6de2cc4f9bbbcee968ce9822cbb0a225286d3961
Oyabure Zakari <oyabure205@gmail.com>
2025-02-23 20:55:29 +0100

    rename component from Error to ShowError

--- components/dictionary/ShowError.tsx
+++ components/dictionary/ShowError.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Alert, View } from "react-native";
 import { useDictionaryStore } from "@/store/useDictionaryStore";
 
-export default function Error() {
+export default function ShowError() {
   const error = useDictionaryStore((state) => state.error);
 
   return <View>{error && Alert.alert("Error", error, [{ text: "OK" }])}</View>;
