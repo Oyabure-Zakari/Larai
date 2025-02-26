a7cd397c9f88a8d6e1e9951f0a170319d9e46ad6
Oyabure Zakari <oyabure205@gmail.com>
2025-02-26 16:43:05 +0100

    fix conditional rendering issue

--- components/translation/Loading.tsx
+++ components/translation/Loading.tsx
@@ -1,24 +1,28 @@
-import React from 'react'
+import React from "react";
 
-import { StyleSheet, View } from 'react-native'
-import { ActivityIndicator } from 'react-native'
+import { StyleSheet, View } from "react-native";
+import { ActivityIndicator } from "react-native";
 
-import { COLORS } from '@/constants/colors'
+import { COLORS } from "@/constants/colors";
 
-import { useTranslationStore } from '@/store/useTranslationStore';
+import { useTranslationStore } from "@/store/useTranslationStore";
 
 export default function Loading() {
   const isLoading = useTranslationStore((state) => state.isLoading);
 
   return (
-    <View style={styles.Loading}> 
-      {isLoading && <ActivityIndicator size={"large"} color={COLORS.green} />}
-    </View>
-  )
+    <>
+      {isLoading && (
+        <View style={styles.Loading}>
+          <ActivityIndicator size={"large"} color={COLORS.green} />
+        </View>
+      )}
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
   Loading: {
     marginTop: 60,
-  }
-})
+  },
+});
