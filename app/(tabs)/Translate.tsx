0618b8f4e6cce0cd939f1c1d471802b2f0774721
Oyabure Zakari <oyabure205@gmail.com>
2025-02-27 15:41:41 +0100

    implement get image from gallery feature

--- app/(tabs)/Translate.tsx
+++ app/(tabs)/Translate.tsx
@@ -1,14 +1,17 @@
 import { StyleSheet, Text} from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { FONT_SIZE } from '@/constants/fonts'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { FloatingAction } from "react-native-floating-action";
 import { Alert } from 'react-native';
 
+import * as ImagePicker from 'expo-image-picker';
+
 import Entypo from '@expo/vector-icons/Entypo';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import { COLORS } from '@/constants/colors';
+import { Image } from 'react-native-elements';
 
 const actions = [
   {
@@ -28,13 +31,31 @@ const actions = [
 ];
 
 export default function Translate() {
+  const [image, setImage] = useState<string | null>(null);
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: 'images',
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log("results", result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
   const handlePress = (name?:string) => {
     switch (name) {
       case "bt_Camera":
         Alert.alert("Camera button pressed!");
         break;
       case "bt_Gallery":
-        Alert.alert("Gallery button pressed!");
+        pickImage()
         break;
       default:
         console.log(`Unknown button: ${name}`);
@@ -44,6 +65,9 @@ export default function Translate() {
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.text}>Translate</Text>
+
+      {image && <Image source={{ uri: image }} style={styles.image} />}
+
       <FloatingAction floatingIcon={<MaterialCommunityIcons name="image-auto-adjust" size={35} color={COLORS.backgroundColor} />} color={COLORS.green} actions={actions} onPressItem={handlePress}/>
     </SafeAreaView>
   )
@@ -59,5 +83,10 @@ const styles = StyleSheet.create({
     text: {
       fontSize: FONT_SIZE.mainText_Seoge.large,
       fontFamily: "segoeui_bold"
-    }
+    },
+
+    image: {
+      width: 200,
+      height: 200,
+    },
 })
