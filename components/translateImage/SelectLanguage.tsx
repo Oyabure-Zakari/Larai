192f008aa686f8d18a9a6f8c52d2904f6301675e
Oyabure Zakari <oyabure205@gmail.com>
2025-03-15 11:58:08 +0100

    create component for SelectLanguage

--- components/translateImage/SelectLanguage.tsx
+++ components/translateImage/SelectLanguage.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-import { COLORS } from "@/constants/colors";
 import { Picker } from "@react-native-picker/picker";
 
 import { useExtractTextTranslationStore } from "@/store/useExtractTextTranslationStore";
+import { COLORS } from "@/constants/colors";
 
 export default function SelectLanguage() {
   const image = useExtractTextTranslationStore((state) => state.image);
@@ -16,43 +16,45 @@ export default function SelectLanguage() {
   );
 
   return (
-    image && (
-      <View style={styles.translateToView}>
-        <Text style={styles.toText}>To: </Text>
-        <Picker
-          selectedValue={translateTo}
-          onValueChange={setTranslateTo}
-          style={styles.picker}
-        >
-          <Picker.Item label="Select a language" value="" enabled={false} />
-          <Picker.Item label="English 🇬🇧🇺🇸🇦🇺" value="en" />
-          <Picker.Item label="Chinese (Mandarin) 🇨🇳" value="zh" />
-          <Picker.Item label="Spanish 🇪🇸🇲🇽🇦🇷" value="es" />
-          <Picker.Item label="Hindi 🇮🇳" value="hi" />
-          <Picker.Item label="Arabic 🇸🇦🇪🇬🇦🇪" value="ar" />
-          <Picker.Item label="French 🇫🇷🇨🇦🇧🇪" value="fr" />
-          <Picker.Item label="Bengali 🇧🇩🇮🇳" value="bn" />
-          <Picker.Item label="Portuguese 🇵🇹🇧🇷" value="pt" />
-          <Picker.Item label="Russian 🇷🇺" value="ru" />
-          <Picker.Item label="Urdu 🇵🇰🇮🇳" value="ur" />
-          <Picker.Item label="German 🇩🇪🇦🇹🇨🇭" value="de" />
-          <Picker.Item label="Japanese 🇯🇵" value="ja" />
-          <Picker.Item label="Swahili 🇰🇪🇹🇿🇺🇬" value="sw" />
-          <Picker.Item label="Italian 🇮🇹" value="it" />
-          <Picker.Item label="Turkish 🇹🇷" value="tr" />
-          <Picker.Item label="Korean 🇰🇷" value="ko" />
-          <Picker.Item label="Tamil 🇮🇳🇱🇰🇸🇬" value="ta" />
-          <Picker.Item label="Persian (Farsi) 🇮🇷" value="fa" />
-          <Picker.Item label="Vietnamese 🇻🇳" value="vi" />
-          <Picker.Item label="Polish 🇵🇱" value="pl" />
-          <Picker.Item label="Dutch 🇳🇱🇧🇪" value="nl" />
-          <Picker.Item label="Filipino (Tagalog) 🇵🇭" value="tl" />
-          <Picker.Item label="Thai 🇹🇭" value="th" />
-          <Picker.Item label="Greek 🇬🇷" value="el" />
-          <Picker.Item label="Hebrew 🇮🇱" value="he" />
-        </Picker>
-      </View>
-    )
+    <>
+      {image && (
+        <View style={styles.translateToView}>
+          <Text style={styles.toText}>To: </Text>
+          <Picker
+            selectedValue={translateTo}
+            onValueChange={setTranslateTo}
+            style={styles.picker}
+          >
+            <Picker.Item label="Select a language" value="" enabled={false} />
+            <Picker.Item label="English 🇬🇧🇺🇸🇦🇺" value="en" />
+            <Picker.Item label="Chinese (Mandarin) 🇨🇳" value="zh" />
+            <Picker.Item label="Spanish 🇪🇸🇲🇽🇦🇷" value="es" />
+            <Picker.Item label="Hindi 🇮🇳" value="hi" />
+            <Picker.Item label="Arabic 🇸🇦🇪🇬🇦🇪" value="ar" />
+            <Picker.Item label="French 🇫🇷🇨🇦🇧🇪" value="fr" />
+            <Picker.Item label="Bengali 🇧🇩🇮🇳" value="bn" />
+            <Picker.Item label="Portuguese 🇵🇹🇧🇷" value="pt" />
+            <Picker.Item label="Russian 🇷🇺" value="ru" />
+            <Picker.Item label="Urdu 🇵🇰🇮🇳" value="ur" />
+            <Picker.Item label="German 🇩🇪🇦🇹🇨🇭" value="de" />
+            <Picker.Item label="Japanese 🇯🇵" value="ja" />
+            <Picker.Item label="Swahili 🇰🇪🇹🇿🇺🇬" value="sw" />
+            <Picker.Item label="Italian 🇮🇹" value="it" />
+            <Picker.Item label="Turkish 🇹🇷" value="tr" />
+            <Picker.Item label="Korean 🇰🇷" value="ko" />
+            <Picker.Item label="Tamil 🇮🇳🇱🇰🇸🇬" value="ta" />
+            <Picker.Item label="Persian (Farsi) 🇮🇷" value="fa" />
+            <Picker.Item label="Vietnamese 🇻🇳" value="vi" />
+            <Picker.Item label="Polish 🇵🇱" value="pl" />
+            <Picker.Item label="Dutch 🇳🇱🇧🇪" value="nl" />
+            <Picker.Item label="Filipino (Tagalog) 🇵🇭" value="tl" />
+            <Picker.Item label="Thai 🇹🇭" value="th" />
+            <Picker.Item label="Greek 🇬🇷" value="el" />
+            <Picker.Item label="Hebrew 🇮🇱" value="he" />
+          </Picker>
+        </View>
+      )}
+    </>
   );
 }
 
